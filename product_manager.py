876fe9f5031f6a139bbc2173d4747a1ff2aa4d6d
ialexbaltag <alexbaltag@outlook.com>
2026-03-11 11:48:56 +0200

    Modificare metodelor din clasa Product, din fisierul product.py

---
+++ product_manager.py
@@ -0,0 +1,16 @@
+class ProductManager:
+    
+    product_list=[]
+    
+    
+    def add_products():
+        pass
+    
+    def info_total_products():
+        pass
+    
+    
+    def total_values_products():
+        pass
+    
+    
