ec2e0f05996122e0de66efdc73f84a63a6bd1fb2
ialexbaltag <alexbaltag@outlook.com>
2026-03-11 12:02:48 +0200

    Modificare metodelor din clasa Product, din fisierul product.py

--- product.py
+++ product.py
@@ -3,10 +3,18 @@ class Product:
     price=0.0
     quantity=0
     
+    def __init__(self, name, price, quantity):
+        self.name=name
+        self.price=price
+        self.quantity=quantity
     
-    def info_about_product():
-        pass
+    
+    def info_about_product(self):
+        return f"Product: {self.name}, Price: {self.price} euros , Quantity: {self.quantity}"
     
     
-    def update_quantity_products():
-        pass
+    def update_quantity_products(self, amount):
+        if self.quantity + amount < 0:
+            print("Stock negative!")
+        else:
+            self.quantity += amount
