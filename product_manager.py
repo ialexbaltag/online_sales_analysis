1a1274ec154946926debca42f47c6f43c125704a
ialexbaltag <alexbaltag@outlook.com>
2026-03-11 12:15:16 +0200

    Modificare in  clasa ProductManager a metodelor din fisierul product_manager.py

--- product_manager.py
+++ product_manager.py
@@ -2,15 +2,21 @@ class ProductManager:
     
     product_list=[]
     
-    
-    def add_products():
-        pass
-    
-    def info_total_products():
-        pass
+    def __init__(self):
+        self.product_list = []
     
     
-    def total_values_products():
-        pass
+    def add_products(self, product):
+        self.product_list.append(product)
+
+    
+    def display_total_products(self):
+        return len(self.product_list)
+    
+    def total_values_products(self):
+        total = 0
+        for product in self.product_list:
+            total += product.price * product.quantity
+        return total
     
     
