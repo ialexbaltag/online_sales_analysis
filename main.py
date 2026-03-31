4071f2d69ec4fea1772df3f211f95f40879c526c
ialexbaltag <alexbaltag@outlook.com>
2026-03-31 22:10:31 +0300

    Am modificat produsele si cantitatea lor in fisierul main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from product_manager import ProductManager
 productsInventory = ProductManager()
 
     # products 
-p1 = Product("Laptop", 1200, 5)
-p2 = Product("Mouse", 25, 20)
-p3 = Product("Keyboard", 70, 10)
+p1 = Product("Smartphone", 1200, 3)
+p2 = Product("SmartTV", 3400, 3)
+p3 = Product("USB Drive", 25, 10)
 
     # add products
 productsInventory.add_products(p1)
@@ -15,15 +15,15 @@ productsInventory.add_products(p2)
 productsInventory.add_products(p3)
 
     # display products
-print("Inventory products:")
-for product in productsInventory.product_list:
-    print(product.info_about_product())
+# print("Inventory products:")
+# for product in productsInventory.product_list:
+#     print(product.info_about_product())
 
     # total products
 print("\nTotal number of products:", productsInventory.display_total_products())
 
-    # total value stock
-print("Total inventory value:", productsInventory.total_values_products(), "euros")
+#     # total value stock
+# print("Total inventory value:", productsInventory.total_values_products(), "euros")
 
 
 
